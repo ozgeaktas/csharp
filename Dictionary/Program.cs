46c9bfce93cd460a10996b4fbf6ca7f7b496dbe4
ozgeaktas <54955167+ozgeaktas@users.noreply.github.com>
2021-01-25 03:05:21 +0300

    C#-MYDİCTİONARY

---
+++ Dictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(123, "Özge");
+            
+        }
+    }
+    
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+        TKey[] _tempkey;
+        TValue[] _tempvalue;
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempkey = _keys;
+            _keys = new TKey[_keys.Length + 1];
+            for(int i = 0; i < _tempkey.Length; i++)
+            {
+                _keys[i] = _tempkey[i];
+            }
+            _keys[_keys.Length - 1] = key;
+
+            _tempvalue = _values;
+            _values = new TValue[_values.Length + 1];
+            for(int i=0; i < _tempvalue.Length; i++)
+            {
+                _values[i] = _tempvalue[i];
+            }
+            _values[_values.Length - 1] = value;
+        }
+    }
+}
